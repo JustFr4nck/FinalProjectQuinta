206ed510ce068f5cd24ac0ac1edf573363e495ce
JustFr4nck <francyperry07@gmail.com>
2026-05-10 21:13:41 +0200

    starting testing with phpUNIT

--- BackEnd/php/tests/MovimentiTest.php
+++ BackEnd/php/tests/MovimentiTest.php
@@ -4,13 +4,35 @@ use PHPUnit\Framework\TestCase;
 
 final class MovimentiTest extends TestCase{
 
-    public function test_function()
-    {
-        // Test
+    public function testIndexReturns404WhenNoTransactionsFound() {
+        // Mock del DB
+        $mysqliMock = $this->createMock(mysqli::class);
+        $stmtMock = $this->createMock(mysqli_stmt::class);
+        $resultMock = $this->createMock(mysqli_result::class);
+
+        //il database restituisce 0 righe
+        $resultMock->num_rows = 0; 
+        $stmtMock->method('get_result')->willReturn($resultMock);
+        $mysqliMock->method('prepare')->willReturn($stmtMock);
+
+        // Mock della Request e Response 
+        $request = (new RequestFactory())->createRequest('GET', '/accounts/1/transactions');
+        $response = (new ResponseFactory())->createResponse();
+        $args = ['idAccount' => 1];
+
+        // Run del Controller
+        $controller = new TransactionController($mysqliMock);
+        $resultResponse = $controller->index($request, $response, $args);
+
+        $this->assertEquals(404, $resultResponse->getStatusCode());
+        
+        $body = (string)$resultResponse->getBody();
+        $this->assertStringContainsString("ERRORE: nessun movimento trovato", $body);
     }
-
-
 }
+
+
+
 
 
 ?>
