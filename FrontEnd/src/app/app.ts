29b41431d818c3dd4da513847ac3f464ccca1c50
JustFr4nck <francyperry07@gmail.com>
2026-05-10 20:55:51 +0200

    Cursor animation added

--- FrontEnd/src/app/app.ts
+++ FrontEnd/src/app/app.ts
@@ -1,15 +1,20 @@
-import { Component, signal } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Component, signal, HostListener } from '@angular/core';
+import {RouterOutlet } from '@angular/router';
 import { MyNavbar } from './components/my-navbar/my-navbar';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, MyNavbar, RouterLinkActive, RouterLink],
+  imports: [CommonModule, RouterOutlet, MyNavbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('FrontEnd');
-  
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(e: MouseEvent) {
+    document.documentElement.style.setProperty('--mouse-x', e.clientX + 'px');
+    document.documentElement.style.setProperty('--mouse-y', e.clientY + 'px');
+  }
+
 }
